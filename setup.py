03152cce1a7b38fc0bdb3d2bc6d84da639556868
mywyau <47248104+mywyau@users.noreply.github.com>
2024-07-09 18:30:51 +0100

    try version 0.0.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='boxcat',
-    version="0.0.9",
+    version="0.0.10",
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
